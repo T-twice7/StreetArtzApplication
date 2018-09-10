1d4a29f742ac98826b7a9ccd12d851cc76c25ae2
JanetZango <39265691+JanetZango@users.noreply.github.com>
2018-09-10 14:49:08 +0200

    change

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,8 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
 import { StreetartzProvider } from '../../providers/streetartz/streetartz';
 import { ModalController,ViewController } from 'ionic-angular';
-
-
+import { obj } from '../../class';
+import { LoadingController } from 'ionic-angular';
 /**
  * Generated class for the LoginPage page.
  *
@@ -18,8 +18,8 @@ import { ModalController,ViewController } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl: ModalController, public viewCtrl: ViewController) {
+  obj = {} as obj
+  constructor(public navCtrl: NavController, public navParams: NavParams,public modalCtrl: ModalController, public viewCtrl: ViewController,public art: StreetartzProvider,public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
@@ -33,5 +33,19 @@ export class LoginPage {
   }
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+  login(obj:obj) {
+    this.art.login(this.obj.email,this.obj.password ).then(()=>{
+     this.presentLoading();
+    } , (error)=>{
+      alert(error)
+    })
+  }
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "signing in....",
+      duration: 3000
+    });
+    loader.present();
   }
 }
--- src/providers/streetartz/streetartz.ts
+++ src/providers/streetartz/streetartz.ts
@@ -3,25 +3,17 @@ import { Injectable } from '@angular/core';
 import { LoginPage } from '../../pages/login/login';
 import { obj } from '../../class';
 import { ToastController } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 declare var firebase;
 @Injectable()
 export class StreetartzProvider {
-  obj = {} as obj
+  obj = {} as obj;
 
-  constructor(public toastCtrl: ToastController) {
+  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
     console.log('Hello StreetartzProvider Provider');
   }
 
-
-  // register(obj: obj) {
-  //   if (obj.password != obj.confirmPassword) {
-  //     this.presentToast1();
-  //   }
-  //     return firebase.auth().createUserWithEmailAndPassword(obj.email, obj.password); 
- 
-
-  // }
   presentToast1() {
     const toast = this.toastCtrl.create({
       message: 'email or password doesnot match!',
@@ -30,21 +22,39 @@ export class StreetartzProvider {
     toast.present();
   }
   register(obj: obj) {
-    if ( obj.password != obj.confirmPassword) {
-      // this.messege = 'email or password doesnot match!';
-      this.presentToast1();
-    } else {
- 
-      return firebase.auth().createUserWithEmailAndPassword(obj.email, obj.password).then((newUser) => {
-        firebase.auth().signInWithEmailAndPassword(obj.email,obj.password).then((authenticatedUser) => {
-          var user = firebase.auth().currentUser
-          firebase.database().ref("profiles/" + user.uid).set(obj);
-          // this.navCtrl.setRoot(MainPage);
- 
+    return firebase.auth().createUserWithEmailAndPassword(obj.email,obj.password).then((newUser) => {
+      firebase.auth().signInWithEmailAndPassword(obj.email,obj.password).then((authenticatedUser) => {
+        var user = firebase.auth().currentUser
+        firebase.database().ref("profiles/" + user.uid).set(obj);
+        // this.navCtrl.setRoot(MainPage);
+      }).catch((error) => {
+        const alert = this.alertCtrl.create({
+          title: error.code,
+          subTitle: error.message,
+          buttons: [
+            {
+              text: 'ok',
+              handler: data => {
+                console.log('Cancel clicked');
+              }
+            }
+          ]
+        });
+        alert.present();
+        console.log(error);
+
       })
- 
     })
-    }
+
+  }
+ login(email , password){
+    return new Promise((resolve, reject)=>{
+      firebase.auth().signInWithEmailAndPassword(email , password).then(()=>{
+        resolve()
+      }, Error =>{
+        alert(Error)
  
-  } 
+      }) ;
+    })
+  }
 }
